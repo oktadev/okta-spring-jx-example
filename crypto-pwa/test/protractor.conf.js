f4593e1d94db342c2b04c228b076db8aca264aca
Matt Raible <matt.raible@okta.com>
2018-07-11 02:32:56 -0600

    Add stage for end-to-end tests

--- crypto-pwa/test/protractor.conf.js
+++ crypto-pwa/test/protractor.conf.js
@@ -6,10 +6,13 @@ exports.config = {
     '../e2e/**/*.e2e-spec.ts'
   ],
   capabilities: {
-    'browserName': 'chrome'
+    'browserName': 'chrome',
+    'chromeOptions': {
+      'args': ['--headless', '--disable-gpu', '--no-sandbox', '--disable-extensions', '--disable-dev-shm-usage']
+    }
   },
   directConnect: true,
-  baseUrl: (process.env.TRAVIS) ? 'http://localhost:8080' : 'http://localhost:8100',
+  baseUrl: (process.env.CI) ? 'http://localhost:' + process.env.PORT : 'http://localhost:8100',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
